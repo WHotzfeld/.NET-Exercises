77c0714f92f4ae26af36d622e1626d1e51583510
Mr-Something <43162563+Mr-Something@users.noreply.github.com>
2018-11-18 17:52:03 -0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WelcomeACME_Drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"Accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
