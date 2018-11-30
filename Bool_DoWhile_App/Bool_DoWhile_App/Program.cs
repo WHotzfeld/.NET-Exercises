420969b9baedb2cb313f9e13dbdb65ed5fce773c
Mr-Something <43162563+Mr-Something@users.noreply.github.com>
2018-11-30 14:27:14 -0800

    Add Initial DoWhile_App and While_App code

---
+++ Bool_DoWhile_App/Bool_DoWhile_App/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Bool_DoWhile_App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("POP QUIZ! What was President Washington's first name? (Include correct capitalization!)");
+            string ThePrez = Console.ReadLine();
+            bool prezAnswer = ThePrez == "George";
+
+            do
+            {
+                switch (ThePrez)
+                {
+                    case "george":
+                        Console.WriteLine("Correct, but you forgot capitalization! Score: 75/100.");
+                        prezAnswer = true;
+                        break;
+
+                    case "George":
+                        Console.WriteLine("Nice! You passed 1st Grade US Social Studies! Score: 100/100.");
+                        prezAnswer = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Nope! Sorry, you failed. Score: 0/100.");
+                        Console.WriteLine("Would you like to try again? If so, type your new answer below:");
+                        ThePrez = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!prezAnswer);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Boolean_While_App/Boolean_While_App/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Boolean_While_App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type a whole numeral. If it's less than 20, I'll count up to 20 for you!");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool numCount = number == 20;
+
+            while (!numCount)
+            {
+                number = (number + 1);
+                Console.WriteLine(number);
+                
+                if (number == 20)
+                {
+                    Console.WriteLine("And there you have it! That's how to count from your number to 20.");
+                    Console.ReadLine();
+                }
+                else if (number > 20)
+                {
+                    Console.WriteLine("It's over 20!");
+                    Console.ReadLine();
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
