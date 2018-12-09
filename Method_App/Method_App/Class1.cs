70504581c4eefdf7484962445a7691ef554c508e
Wesley Hotzfeld <wesley.hotzfeld@gmail.com>
2018-12-08 19:30:53 -0800

    Add initial Method_App code

---
+++ Method_App/Method_App/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_App
+{
+    class Class1
+    {
+        public static int AddFive(int numInput)
+        {
+            int FirstNumber = numInput + 5;
+            return FirstNumber;
+        }
+
+        public static int AddTen(int numInput)
+        {
+            int secondNumber = numInput + 10;
+            return secondNumber;
+        }
+
+        public static int AddTwenty(int numInput)
+        {
+            int thirdNumber = numInput + 20;
+            return thirdNumber;
+        }
+    }
+}
---
+++ Method_App/Method_App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole numeral to be operated on.");
+            Console.WriteLine("I will produce three new numerals: your numeral added to 5, your numeral added to 10, and your numeral added to 20.");
+            int numInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Class1.AddFive(numInput));
+            Console.WriteLine(Class1.AddTen(numInput));
+            Console.WriteLine(Class1.AddTwenty(numInput));
+
+            Console.ReadLine();
+        }
+    }
+}
