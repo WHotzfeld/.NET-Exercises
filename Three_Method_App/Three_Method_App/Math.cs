0795f86c9d30ede98f5eb8210b146f6d5c7c5f0a
Mr-Something <43162563+Mr-Something@users.noreply.github.com>
2018-12-12 12:27:59 -0800

    Add initial Three_Method_App code

---
+++ Three_Method_App/Three_Method_App/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Three_Method_App
+{
+    class Math
+    {
+        //STEP 1.) Created "Math" class. Created method "FirstThing" that takes an integer and returns an integer.
+        public static int FirstThing(int inputInt)
+        {
+            int displayInt = inputInt * 2;
+            return displayInt;
+        }
+
+        //STEP 3.) Created second method "FirstThing" that takes a decimal and returns an integer.
+        public static int FirstThing(decimal inputDec)
+        {
+            int displayDec = Convert.ToInt32(inputDec) + 2;
+            return displayDec;
+        }
+
+        //STEP 5.) Created third method "FirstThing" that takes a string, converts it to an integer if possible,
+        // and returns an integer.
+        public static int FirstThing(string inputStr)
+        {
+            int displayStr = Convert.ToInt32(inputStr) - 2;
+            return displayStr;
+        }
+    }
+}
---
+++ Three_Method_App/Three_Method_App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Three_Method_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STEP 2.) Instantiated "multiplication" Math object. Called first method and displayed result.
+            Math multiplication = new Math();
+            
+            Console.WriteLine(Math.FirstThing(inputInt: 12));
+            Console.ReadLine();
+
+            //STEP 4.) Instantiated "addition" Math object. Called second method and displayed result as int.
+            Math addition = new Math();
+
+            Console.WriteLine(Math.FirstThing(inputDec: 15.0m));
+            Console.ReadLine();
+
+            //STEP 6.) Instantiated "subtraction" Math object. Called third method and displayed result as int.
+            Math subtraction = new Math();
+
+            Console.WriteLine(Math.FirstThing(inputStr: "20"));
+            Console.ReadLine();
+        }
+    }
+}
