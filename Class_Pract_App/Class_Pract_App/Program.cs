edb11122f82a424c0dc1e79b0aa709e7f2d9b2e7
Mr-Something <43162563+Mr-Something@users.noreply.github.com>
2018-12-11 20:07:40 -0800

    Add initial Class_Pract_App code

---
+++ Class_Pract_App/Class_Pract_App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Pract_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STEP 2.) Instantiated "theStuff."
+            Thing theStuff = new Thing();
+
+            //STEP 3.) Called method with 2 numbers.
+            Thing.Operation(3, 15);
+
+            //Step 4.) Called method with named parameters.
+            Thing.Operation(firstNum: 5, secondNum: 6);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class_Pract_App/Class_Pract_App/Thing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Pract_App
+{
+    class Thing
+    {
+        //STEP 1.) Class "Thing." Two integer parameters "firstNum" and "secondNum."
+        public static void Operation(int firstNum, int secondNum)
+        {
+            int mathAnswer = firstNum * 10;
+            Console.WriteLine(secondNum);
+        }
+    }
+}
