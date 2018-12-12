26cd6a72b1d0827448d59efada6430c0fc7314dd
Mr-Something <43162563+Mr-Something@users.noreply.github.com>
2018-12-12 13:58:31 -0800

    Add initial Method_Optional_App code

---
+++ Method_Optional_App/Method_Optional_App/Magic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Method_Optional_App
+{
+    class Magic
+    {
+        //STEP 1.) Created "Magic" class. Created "Calc" method that takes two integers, one optional, and returns an integer.
+        //NOTE: The second integer has a default, but the default may be superfluous thanks to additional code in the main().
+        // Said code is noted on main().
+        public static int Calc(int firstInput, int secondInput = 0)
+        {
+            int calcAnswer = 20 + firstInput + secondInput;
+            return calcAnswer;
+        }
+    }
+}
---
+++ Method_Optional_App/Method_Optional_App/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Method_Optional_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STEP 2.) Instantiated "Planeswalker" object.
+            Magic Planeswalker = new Magic();
+
+            //STEP 3.) Asked for user input of one integer with the option for a second integer.
+            Console.WriteLine("Please type an integer you would like magic performed on, then press \"Enter.\"");
+            int firstInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("If you would like a second integer to be a part of the magic, type it here and press \"Enter.\"");
+            Console.WriteLine("If you are fine with just one integer and have no other integer, simply press \"Enter\" without input.");
+
+            //NOTE: I have a default for "secondInput" already set in the method, but ran into trouble when asking for input.
+            // My original code only had the line within the "try" (the Convert...ReadLine), but I hit a format error whenever
+            // the user didn't enter anything. This is the first thing I tried that worked. I'm sure there's a simpler way to
+            // handle it, but it's beyond me! At any rate, the try/catch accomplishes the goal of the app.
+            int secondInput = 0;
+            try
+            {
+                secondInput = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(FormatException)
+            {
+                secondInput = 0;
+            }
+            //STEP 4.) Called the "Calc" method, allowing for one or two separate integers.
+            Console.WriteLine("Wowwy zowwy! I did magic things and now your integer(s) have become: " + Magic.Calc(firstInput, secondInput) + "!");
+            
+            Console.ReadLine();
+
+            //STEP 5.) Tried several integer combinations, as well as only inputting one integer. Seems to work well.
+        }
+    }
+}
