1a6a97ff6d967ff262a7ff0ef80bc1df710d3233
Mr-Something <43162563+Mr-Something@users.noreply.github.com>
2018-12-26 14:45:04 -0600

    Add initial Struct_App code

---
+++ Struct_App/Struct_App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STEP 2.) Created new Number "number" and set "number" Amount to 12.5.
+            Number number = new Number();
+            number.Amount = 12.5m;
+
+            //STEP 3.) Printed Amount for "number" to console.
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+        //STEP 1.) Created struct Number with "Amount" as decimal data type.
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
