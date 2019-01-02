84b6c287579a935e19c7ccdf9fbffbe5f891b403
Mr-Something <43162563+Mr-Something@users.noreply.github.com>
2019-01-02 15:29:32 -0800

    Add initial DateTime_Log_App code

---
+++ DateTime_Log_App/DateTime_Log_App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Log_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STEP 1.) Created datetime object, printed current datetime to console.
+            DateTime rightNow = DateTime.Now;
+
+            //STEP 2.) Asked user the amount of time they will waste by staring at a console window.
+            Console.WriteLine("The current date and time is " + rightNow + ". \n" +
+                "How many hours do you plan on staring at this screen?");
+            int longWait = Convert.ToInt32(Console.ReadLine());
+
+            //STEP 3.) Printed the expected completion datetime and provided user with sage life advice.
+            Console.WriteLine("Wow, really? When you're done, it will be " + rightNow.AddHours(longWait) + ". \n" +
+                "That's a long time. Maybe just do something else for the next " + longWait + " hours instead.");
+            Console.ReadLine();
+        }
+    }
+}
