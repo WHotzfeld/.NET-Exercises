0bc0fc63d54900f6e9c89105efb33177d70791e7
Mr-Something <43162563+Mr-Something@users.noreply.github.com>
2019-01-02 14:55:12 -0800

    Add initial IO_Log_App code

---
+++ IO_Log_App/IO_Log_App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IO_Log_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STEP 1.) Asked user for number, converted to int.
+            Console.WriteLine("Please enter a number:");
+            int theNumber = Convert.ToInt32(Console.ReadLine());
+            
+            //STEP 2.) Created StreamWriter, logged int input.
+            using (StreamWriter file = new StreamWriter(@"C:\Users\wesle\Logs\log.txt", true))
+            {
+                file.WriteLine(theNumber);
+            }
+            
+            //STEP 3.) Printed entirety of log.txt.
+            string logText = File.ReadAllText(@"C:\Users\wesle\Logs\log.txt");
+            Console.WriteLine(logText);
+            Console.ReadLine();
+        }
+    }
+}
