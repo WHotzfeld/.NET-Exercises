cd38cfaf3314a4a5a618f0a4b1705a3d456bba2e
Mr-Something <43162563+Mr-Something@users.noreply.github.com>
2019-01-03 11:07:57 -0800

    Add initial Add_Feat_App code

---
+++ Add_Feat_App/Add_Feat_App/Panda.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Feat_App
+{
+    public class Panda
+    {
+        //STEP [all].) Created parameters and constructor for Panda class.
+        public string Name { get; set; }
+        public string ColorOne { get; set; }
+        public string ColorTwo { get; set; }
+        public string Species { get; set; }
+
+        public Panda(string name, string furColor1, string furColor2, string species)
+        {
+            Name = name;
+            ColorOne = furColor1;
+            ColorTwo = furColor2;
+            Species = species;
+        }
+
+        //STEP 3 (cont). Created constructor call chain.
+        public Panda(string name) : this(name, "Black", "White", "Panda Bear")
+        {
+        }
+        
+    }
+}
---
+++ Add_Feat_App/Add_Feat_App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Feat_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STEP 1.) Created constant string "species".
+            const string species = "Panda Bear";
+
+            //STEP 2.) Created var object from Panda class. (Also, used const "species" to test STEP 1.)
+            var firstPanda = new Panda("Xiao Mei", "Brown", "White", species);
+
+            //STEP 3.) Created new object from Panda class using constructor call chain.
+            Panda secondPanda = new Panda("Lum Lum");
+
+            //STEP X.) Printed all panda info to console to check work.
+            var Pandas = new List<Panda> { firstPanda, secondPanda };
+
+            foreach(Panda panda in Pandas)
+            {
+                Console.WriteLine(panda.Name + " : " + panda.Species + ", " + panda.ColorOne + " & " + panda.ColorTwo + ".");
+            }
+            Console.ReadLine();
+        }
+    }
+}
